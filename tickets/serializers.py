fc0fca548ac0c2b80f3537c7eb380b694e713482
DgrinderHZ <hassanzekkouri@gmail.com>
2021-10-19 22:02:48 +0100

    Update serializers.py

--- tickets/serializers.py
+++ tickets/serializers.py
@@ -2,19 +2,19 @@ from rest_framework import serializers as serial
 from .models import Guest, Movie, Reservation
 
 
-class GuestSeriaizer(serial.ModelSerializer):
+class GuestSerializer(serial.ModelSerializer):
     class Meta:
         model = Guest
         fields = ['id', 'name', 'mobile', 'reservations']
 
 
-class MovieSeriaizer(serial.ModelSerializer):
+class MovieSerializer(serial.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
 
 
-class ReservationSeriaizer(serial.ModelSerializer):
+class ReservationSerializer(serial.ModelSerializer):
     class Meta:
         model = Reservation
         fields = '__all__'
